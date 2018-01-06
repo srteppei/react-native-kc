6fbe4f9a4956da4202767ce28b12eb205a48a6f5
Jesus <jesusfarmaciaribera@gmail.com>
2018-01-06 13:32:00 +0100

    Complete charcter list view with redux

--- src/Sections/CharacterList/Template/CharacterCell.js
+++ src/Sections/CharacterList/Template/CharacterCell.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import { TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
 
 export default class CharacterCell extends Component {
 
@@ -10,19 +10,33 @@ export default class CharacterCell extends Component {
 
     render() {
 
-        const { item } = this.props;
-
+        const { item, onSelect} = this.props;
         const id = item.id ? item.id : 'This item has not ID';
         const name = item.name ? item.name : 'This item has not name';
-
+        const image = item.thumbnail ? { uri: item.thumbnail.path + '.' + item.thumbnail.extension } : null;
         return (
-            <TouchableOpacity>
-                <View>
-                    <Text>{id}</Text>
-                    <Text>{name}</Text>
-                </View>
+            <TouchableOpacity style={style.container} onPress={() => onSelect(item)}>
+                <Image style={style.image} source={image} resizeMode={'contain'} />
+                <Text style={style.name}>{name}</Text>
             </TouchableOpacity>
         )
     }
-
 }
+
+const style = StyleSheet.create({
+    container: {
+        margin: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row'
+    },
+    image: {
+        flex: 1,
+        height: 100
+    },
+    name: {
+        flex: 3,
+        fontSize: 18,
+        textAlign: 'center',
+        textAlignVertical: 'center'
+    }
+})
--- src/Sections/CharacterList/Template/CharacterList.js
+++ src/Sections/CharacterList/Template/CharacterList.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import { View, FlatList, Text } from 'react-native';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+import { Actions } from 'react-native-router-flux'
 import CharacterCell from './CharacterCell';
 
-export default class CharacterList extends Component {
+import { connect } from 'react-redux';
+import * as CharacterActions from '../Redux/Action';
+import * as Colors from '../../Commons/style/Colors';
 
-    mock = [{ id: 1, name: 'asdasd' }, { id: 1, name: 'asdasd' }, { id: 1, name: 'asdasd' }, { id: 1, name: 'asdasd' }];
+class CharacterList extends Component {
+    
+    componentWillMount () {
+        this.props.navigation.setParams({title: 'CharacterList'});
+        this.props.fetchCharactersList();
+    }
 
     render() {
-
-        this.props.navigation.setParams({title: 'CharacterList'});
-
         return (
-            <View>
+            <View style={style.container}>
                 <FlatList
-                    data={this.mock}
+                    data={this.props.characterList}
                     renderItem={({ item, index }) => this.renderItem(item, index)}
                     keyExtractor={(item, index) => index}
                     extraData={this.props}
@@ -23,7 +28,34 @@ export default class CharacterList extends Component {
     } 
 
     renderItem(item, index) {
-        return <CharacterCell item={item} />
+        return <CharacterCell item={item} onSelect={(character) => this.props.updateSelected(character)}/>
     }
 
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.blue
+    }
+});
+
+const mapStateToProps = (state) => {
+    return {
+        characterList: state.default.characterList
+    }
+}
+
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        fetchCharactersList: () => {
+            dispatch(CharacterActions.fetchCharacterList())
+        },
+        updateSelected: (character) => {
+            dispatch(CharacterActions.updateCharacterSelected(character));
+            console.log(character);
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CharacterList)
