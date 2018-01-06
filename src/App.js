c1d5aa7489922e9b013ca8ce3613c1dddb5d2342
Jesus <jesusfarmaciaribera@gmail.com>
2018-01-06 13:32:26 +0100

    Implement chracterlist in app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-
 import React, { Component } from 'react';
-import { } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
+
+import * as Colors from './Sections/Commons/style/Colors'
 
 // Components
 import CharacterList from './Sections/CharacterList/Template/CharacterList';
 // Components
 
+// REDUX
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider, connect } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import * as reducers from './Sections/CharacterList/Redux/Reducer' // Nuestros reducers TODO: Implement reducers
+const reducer = combineReducers(reducers) // Combinamos nuestros reducers
+const store = createStore( // Creamos el store con:
+  reducer, // Nuestros reducer
+  applyMiddleware(thunk) // Nuestro middleware redux-thunk
+)
+// REDUX
+
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <Scene key="root">
-          <Scene
-            key={'CharacterList'}
-            component={CharacterList}
-          />
-        </Scene>
-      </Router>
+      <Provider store={store} >
+        <Router>
+          <Scene key="root">
+            <Scene
+              key={'CharacterList'}
+              component={CharacterList}
+              navigationBarStyle={style.navBar}
+              titleStyle={style.titleStyle}
+            />
+          </Scene>
+        </Router>
+      </Provider>
     );
   }
 }
+
+const style = StyleSheet.create({
+  navBar: {
+    backgroundColor: Colors.red,
+  },
+  titleStyle: {
+    color: Colors.white
+  }
+})
